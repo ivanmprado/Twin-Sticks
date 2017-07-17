5b2d5cdc1e5a7bd12e4583f9cc5a0b100f965b2e
Ivan Monteiro Prado <ivanmonteiroprado@gmail.com>
2017-07-17 20:45:16 -0300

    Final Commit Added Camera Pan, GameManager and PauseGame

---
+++ Assets/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GameManager : MonoBehaviour {
+
+    public bool isRecording = true;
+    private bool isPaused = false;
+    private float fixedDeltaTime;
+
+    private void Start() {
+        fixedDeltaTime = Time.fixedDeltaTime;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (CrossPlatformInputManager.GetButton("Fire1")) {
+            isRecording = false;
+        } else {
+            isRecording = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.P) && isPaused) {
+            isPaused = false;
+            ResumeGame();
+        } else if (Input.GetKeyDown(KeyCode.P) && !isPaused) {
+            isPaused = true;
+            PauseGame();
+        }
+
+    }
+
+    void PauseGame(){
+        Time.timeScale = 0f;
+        Time.fixedDeltaTime = 0f;
+    }
+
+    void ResumeGame() {
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = fixedDeltaTime;
+    }
+
+    private void OnApplicationPause(bool pause) {
+        isPaused = pause;
+    }
+}
---
+++ Assets/Replay.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Replay : MonoBehaviour {
+
+    private const int bufferFrames = 100;
+    private MyKeyFrame[] keyFrames = new MyKeyFrame[bufferFrames];
+    private Rigidbody rigidbody;
+    private GameManager manager;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        manager = FindObjectOfType<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (manager.isRecording) {
+            Record();
+        } else {
+            PlayBack();
+        }
+    }
+
+    private void Record() {
+        rigidbody.isKinematic = false;
+        int frame = Time.frameCount % bufferFrames;
+        float time = Time.time;
+        keyFrames[frame] = new MyKeyFrame(time, transform.position, transform.rotation);
+    }
+
+    void PlayBack() {
+        rigidbody.isKinematic = true;
+        int frame = Time.frameCount % bufferFrames;
+        transform.position = keyFrames[frame].position;
+        transform.rotation = keyFrames[frame].rotation;
+    }
+}
+
+public struct MyKeyFrame {
+
+    public float frameTime;
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public MyKeyFrame(float time, Vector3 aPosition, Quaternion aRotation) {
+        frameTime = time;
+        position = aPosition;
+        rotation = aRotation;
+    }
+}
